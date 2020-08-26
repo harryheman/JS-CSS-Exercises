59204e081ff3db461c01229a995f2d479719bc57
Igor <aio350@yahoo.com>
2020-08-26 20:03:14 +0500

    remove

--- webpack/learn-webpack/src/component.js
+++
@@ -1,7 +0,0 @@
-export default (text = 'ok') => {
-    const p = document.createElement('p')
-
-    p.textContent = text
-
-    return p
-}
--- webpack/learn-webpack/src/image-component.js
+++
@@ -1,5 +0,0 @@
-import image from './logo.png'
-
-const img = document.createElement('img')
-img.src = image
-document.body.append(img)

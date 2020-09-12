2342d5ef6104c06819e66590dd03b80f17bb851f
Igor <aio350@yahoo.com>
2020-09-13 02:23:08 +0500

    add

---
+++ personal-blog-gatsby/src/components/article.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Article = ({
+    content: {
+        title,
+        text,
+        publishedAt
+    }
+}) => (
+    <div>
+        <h2>{title}</h2>
+        <p>
+            {text.text}
+        </p>
+        <p>{publishedAt}</p>
+    </div>
+)
+
+export default Article

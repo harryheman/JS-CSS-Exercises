868891e6ab3986f2bc6fd40e21f0e2cebbc87fbb
Igor Agapov <aio350@yahoo.com>
2021-01-03 21:21:27 +0500

    update

---
+++ CLI/todo/src/commands/add.js
@@ -0,0 +1,25 @@
+const { Command } = require('@oclif/command')
+const Todo = require('../db')
+const chalk = require('chalk')
+
+class AddCommand extends Command {
+  async run() {
+    const { argv } = this.parse(AddCommand)
+    try {
+      await Todo.push({
+        id: Todo.value().length,
+        task: argv.join(' '),
+        done: false
+      }).write()
+      this.log(chalk.green('New todo created.'))
+    } catch {
+      this.log(chalk.red('Operation failed.'))
+    }
+  }
+}
+
+AddCommand.description = `Adds a new todo`
+
+AddCommand.strict = false
+
+module.exports = AddCommand

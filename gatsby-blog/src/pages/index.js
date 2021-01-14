d52fa5d85178be25b1e186340070489971fa2833
Igor Agapov <aio350@yahoo.com>
2021-01-14 16:49:27 +0500

    update

---
+++ gatsby-blog/src/pages/index.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Article from "../components/article"
+
+const IndexPage = () => (
+  <Layout>
+    <StaticQuery
+      query={graphql`
+        {
+          contentfulPerson {
+            fullName
+          }
+          allContentfulArticle {
+            edges {
+              node {
+                id
+                title
+                text {
+                  text
+                }
+                publishedAt
+              }
+            }
+          }
+        }
+      `}
+      render={({ allContentfulArticle: { edges } }) =>
+        edges.map(({ node }) => <Article key={node.id} content={node} />)
+      }
+    />
+  </Layout>
+)
+
+export default IndexPage
